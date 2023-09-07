ff7f500367df1422a7a7798b066058ce3f1c936d
zhuyongqiang <981162250@qq.com>
2023-09-07 14:29:35 +0800

    线程池七大参数

--- kube-api/src/main/java/com/huolieniao/config/ThreadPoolConfig.java
+++ kube-api/src/main/java/com/huolieniao/config/ThreadPoolConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * 自定义线程池
@@ -14,15 +16,20 @@ import java.util.concurrent.Executor;
 @EnableAsync
 public class ThreadPoolConfig {
 
+    /**
+     * 七大参数 核心线程 最大线程 队列容量 最大线程空闲时间 线程工厂 拒绝策略
+     * @return
+     */
     @Bean("taskExecutor")
     public Executor taskExecutor() {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
         taskExecutor.setCorePoolSize(10);
         taskExecutor.setMaxPoolSize(50);
-        taskExecutor.setQueueCapacity(200);
+        taskExecutor.setQueueCapacity(1000);
         taskExecutor.setKeepAliveSeconds(60);
         taskExecutor.setThreadNamePrefix("MyThread-");
-        taskExecutor.setAwaitTerminationSeconds(60);
+        taskExecutor.setThreadFactory(Executors.defaultThreadFactory());
+        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         return taskExecutor;
     }
 }
